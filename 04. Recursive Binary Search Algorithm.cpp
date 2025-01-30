7c25a9d113d89e7bea2a1b582c53c0cc37cea8b7
MdJakariaNur <104752446+mdjakarianur@users.noreply.github.com>
2025-01-30 10:39:18 +0600

    Update 04. Recursive Binary Search Algorithm.cpp

--- 04. Recursive Binary Search Algorithm.cpp
+++ 04. Recursive Binary Search Algorithm.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 using namespace std;
-
 int binarySearch(int arr[], int low, int high, int x) {
     if (high >= low) {
         int mid = low + (high - low) / 2;
 
-        if (arr[mid] == x)
+        if (arr[mid] == x){
             return mid;
-
-        if (arr[mid] > x)
+        }
+        if (arr[mid] > x){
             return binarySearch(arr, low, mid - 1, x);
-
+        }
         return binarySearch(arr, mid + 1, high, x);
     }
     return -1;
 }
-
 int main() {
     int arr[] = {2, 3, 4, 10, 40};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 3;
+    int x = 10;
     int result = binarySearch(arr, 0, n - 1, x);
     
     if (result == -1) 
-        cout << "Element is not present in array";
+        cout << "Element is not found";
     else 
-        cout << "Element is present at index " << result;
+        cout << "Element is found at index " << result;
     
     return 0;
 }
